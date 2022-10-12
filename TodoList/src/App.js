18e1d398bc3a6e8c5784b7d21727dadb7f4aa1fd
Prakash <pkpndy@gmail.com>
2022-10-12 18:02:58 +0530

    commit

---
+++ TodoList/src/App.js
@@ -0,0 +1,59 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Plan from './plan';
+
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    items: [],
+    text: ""
+  }
+  handleChange = e => {
+    this.setState({text: e.target.value })
+  }
+  handleAdd = e => {
+    if(this.state.text !== ""){
+      const items = [...this.state.items, this.state.text];
+      this.setState({items: items, text : ""});
+    }
+  }
+  handleDelete = id => {
+    console.log("Deleted", id);
+    const oldItems = [...this.state.items];
+    const Items = oldItems.filter((elements, i) => {
+      return i !== id;
+    })
+    this.setState({items: Items})
+  }
+  render() {
+    return (
+      <div className='container-fluid my-5'>
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
+            <h1 className='text-center'>Today's Plan</h1>
+            <div className="row">
+              <div className="col-9">
+                <input type="text" className='form-control' placeholder='Write Plan Here' 
+                value={this.state.text} onChange={this.handleChange}/>
+              </div>
+              <div className="col-2">
+                <button className="btn btn-warning px-5 fw-bold" onClick={this.handleAdd}>Add</button>
+              </div>
+              <div className="container-fluid">
+                <ul className="list-unstyled row m-5">
+                  {this.state.items.map((value, i) => {
+                    return <Plan key={i} value={value} id={i} sendData={this.handleDelete}/>
+                  })}
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
